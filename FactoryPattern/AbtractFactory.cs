d0877f71d31b130faac1d9fa6aed28c8b07bac0a
Phillip <phillip7.et@gmail.com>
2019-09-11 16:13:14 +0700

    create abstract factory pattern

--- FactoryPattern/AbtractFactory.cs
+++ FactoryPattern/AbtractFactory.cs
@@ -1,11 +1,106 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace DesignPattern.FactoryPattern
+namespace DesignPattern.FactoryPattern.AbstractFactory
 {
-    class AbtractFactory
+    public class AbtractFactory
     {
-        
+
+    }
+    interface IChair
+    {
+        void create();
+    }
+    interface ITable
+    {
+        void create();
+    }
+    interface IFurnitureAbstractFactory
+    {
+        IChair createChair();
+        ITable createTable();
+    }
+
+    public class FurnitureFactory
+    {
+        public static dynamic getFactory(MaterialType type)
+        {
+            IFurnitureAbstractFactory factory = null;
+            switch (type)
+            {
+                case MaterialType.FLASTIC: factory = new PlasticFactory(); break;
+                case MaterialType.WOOD: factory = new WoodFactory(); break;
+                default: throw new NotImplementedException();
+            }
+            return factory;
+        }
+        public static dynamic createFurniture(MaterialType mType, FurnitureType fType)
+        {
+            IFurnitureAbstractFactory factory = null;
+            switch (mType)
+            {
+                case MaterialType.FLASTIC: factory = new PlasticFactory(); break;
+                case MaterialType.WOOD: factory = new WoodFactory(); break;
+                default: throw new NotImplementedException();
+            }
+            dynamic furniture = null;
+            switch (fType)
+            {
+                case FurnitureType.CHAIR: furniture = factory.createChair(); break;
+                case FurnitureType.TABLE: furniture = factory.createTable(); break;
+            }
+            return furniture;
+        }
+    }
+    public class PlasticFactory : IFurnitureAbstractFactory
+    {
+        IChair IFurnitureAbstractFactory.createChair()
+        {
+            return new PlasticChair();
+        }
+
+        ITable IFurnitureAbstractFactory.createTable()
+        {
+            return new PlasticTable();
+        }
+    }
+
+    public class WoodFactory : IFurnitureAbstractFactory
+    {
+        IChair IFurnitureAbstractFactory.createChair()
+        {
+            return new WoodChair();
+        }
+
+        ITable IFurnitureAbstractFactory.createTable()
+        {
+            return new WoodTable();
+        }
+    }
+    public class WoodTable : ITable
+    {
+        void ITable.create()
+        {
+            Console.WriteLine("create a Wood Table");
+        }
+    }
+    public class PlasticTable : ITable
+    {
+        void ITable.create()
+        {
+            Console.WriteLine("create a Plastis Table");
+        }
+    }
+    public class WoodChair : IChair
+    {
+        void IChair.create()
+        {
+            Console.WriteLine("create a Wood Chair");
+        }
+    }
+    public class PlasticChair : IChair
+    {
+        void IChair.create()
+        {
+            Console.WriteLine("create a Plastis Chair");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,8 +1,17 @@
 ﻿using System;
 using DesignPattern.FactoryPattern.SimpleFactory;
 using DesignPattern.FactoryPattern.FactoryMethod;
+using DesignPattern.FactoryPattern.AbstractFactory;
 namespace DesignPattern
 {
+    public enum MaterialType
+    {
+        FLASTIC, WOOD
+    }
+    public enum FurnitureType
+    {
+        CHAIR, TABLE
+    }
     class Program
     {
         static void Main(string[] args)
@@ -15,8 +24,21 @@ namespace DesignPattern
             // ======== FACTORY METHOD ===========
             IBank fmBank = FactoryMethod.getBank("ACBBank");
             Console.WriteLine(fmBank.getBankName());
-            Console.Read();
+            //Console.Read();
 
+
+            // ABSTRACT FACTORY - show screnario create factory and furniture
+            IFurnitureAbstractFactory factory = FurnitureFactory.getFactory(MaterialType.FLASTIC);
+            IChair chair = factory.createChair();
+            chair.create(); 
+            ITable table = factory.createTable();
+            table.create();
+
+            // ABSTRACT FACTORY - hide screnario create factory and furniture
+            // can not use var c = ...
+            IChair c = FurnitureFactory.createFurniture(MaterialType.WOOD, FurnitureType.CHAIR);
+            c.create();
+            Console.Read();
         }
     }
 }
